9226dde542560de14a46b1e441d0fc58275cd88d
Daniel Wagner <dawagner@adobe.com>
2016-06-08 09:52:44 +0200

    Removed functions extracted to separate modules, added completion logic

--- src/index.js
+++ src/index.js
@@ -1,43 +1,14 @@
 import 'babel-polyfill';
 import inquirer from 'inquirer';
 const ArgumentParser = require('argparse').ArgumentParser;
-import { getConfig, arrayify, isPositional, isRequired } from './utils';
+import { arrayify, isPositional, isRequired,
+        getCommandArgs, loadConfig } from './utils';
 import micromatch from 'micromatch';
-import findup from 'findup-sync';
-import path from 'path';
-import resolve from 'resolve';
-import os from 'os';
 import winston from 'winston';
-
-const parentDir = path.dirname(module.parent.filename);
-winston.debug(`CONFIG: parentDir = ${parentDir}`);
-const packageFile = findup('package.json', { cwd: parentDir });
-winston.debug(`CONFIG: packageFile = ${packageFile}`);
-const configFile = findup('.cmdclirc.json', { cwd: parentDir });
-winston.debug(`CONFIG: configFile = ${configFile}`);
-const requireFn = (name) => {
-  // This searches up from the specified package.json file, making sure
-  // the config option behaves as expected. See issue #56.
-  const src = resolve.sync(name, { basedir: path.dirname(packageFile) });
-  return require(src);
-};
-
-const configObject = {
-  ...requireFn(packageFile),
-  ...requireFn(configFile),
-};
-
-const pattern = arrayify(configObject.commandsPattern || ['cmdcli-*', 'cmd-cli-*']);
-const scope = arrayify(configObject.commandsScope ||
-  ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']);
-const LOCAL_FOLDERS = arrayify(configObject.localCommands || []);
-const CONFIG_FILE = configObject.configFile || '.clirc';
-const GLOBAL_CONFIG_FILE = `${os.homedir()}/${CONFIG_FILE}`;
-const BASE_CONFIG = configObject.baseConfig || {};
-const names = scope.reduce((result, prop) => (
-  result.concat(Object.keys(configObject[prop] || {}))
-), []);
-const EMPTY_PASSWORD = '__EMPTY__';
+import { EMPTY_PASSWORD } from './const';
+import { requireFn, names, pattern, LOCAL_FOLDERS, configObject, binEntryPoint,
+        BASE_CONFIG, GLOBAL_CONFIG_FILE, CONFIG_FILE } from './config';
+import complete from './complete';
 
 let commandsClasses;
 LOCAL_FOLDERS.forEach((name) => {
@@ -106,17 +77,6 @@ function mapInquirer(id, cfg, config) {
   return ret;
 }
 
-function mapArgparse(id, cfg) {
-  const isArgRequired = isRequired(id, cfg);
-  return {
-    ...(isArgRequired || cfg.action === 'store' && { help: '(default: %(defaultValue)s)' }),
-    ...(isArgRequired && { nargs: '?', action: 'store' }),
-    ...(!isArgRequired && { action: 'storeTrue' }),
-    ...(cfg.isPassword && { action: 'store', nargs: '?', constant: EMPTY_PASSWORD }),
-    ...cfg,
-  };
-}
-
 function argsSelector(cfg) {
   return new Promise((resolveSelector) => {
     inquirer.prompt(cfg).then((answers) => {
@@ -169,30 +129,6 @@ function defineTopCommand(name, classes, parentParser) {
   );
 }
 
-function *getCommandArgs(command) {
-  let params;
-  if (typeof command.getArgs === 'function') {
-    params = command.getArgs();
-  } else if (Array.isArray(command.args)) {
-    params = command.args;
-  }
-
-  if (params) {
-    for (const param of params) {
-      const p = arrayify(param);
-      const cfg = (typeof p[p.length - 1] === 'object') ?
-        p.pop() : {};
-      const id = (Array.isArray(p[0])) ?
-        p[0] : p;
-
-      yield {
-        id,
-        cfg: mapArgparse(id, cfg),
-      };
-    }
-  }
-}
-
 function defineCommand(parentParser, cmdName, CmdCls) {
   let command;
   const cmdParserCfg = { help: true };
@@ -232,47 +168,44 @@ parser.addArgument(['-g', '--global'], {
 
 const commands = { root: defineTopCommand('root', commandsClasses, parser) };
 
-let args = parser.parseArgs();
-const cmdPath = ['root'];
-let i = 'root';
-let command = commands[i];
-while (!command.isCommand) {
-  i = args[i];
-  cmdPath.push(i);
-  command = command[i];
+if (!complete(binEntryPoint, commands, parser)) {
+  let args = parser.parseArgs();
+  const cmdPath = ['root'];
+  let i = 'root';
+  let command = commands[i];
+  while (!command.isCommand) {
+    i = args[i];
+    cmdPath.push(i);
+    command = command[i];
+  }
+
+  const config = loadConfig(args.global, BASE_CONFIG, GLOBAL_CONFIG_FILE, CONFIG_FILE);
+
+  let resolvedArgs = args;
+
+  if (typeof command.getArgs === 'function' || Array.isArray(command.args)) {
+    resolvedArgs = config.then((cfg) => checkArgs(command, args, cfg));
+  }
+
+  Promise.all([
+    Promise.resolve(resolvedArgs),
+    config,
+  ]).then((result) => {
+    args = result[0];
+    const cfg = result[1];
+
+    Promise.resolve(command.exec(args, cfg))
+      .then(results => {
+        if (typeof command.print === 'function') {
+          command.print(results, args, cfg);
+        }
+      })
+      .catch(err => {
+        if (typeof command.catch === 'function') {
+          command.catch(err, args, cfg);
+        } else {
+          process.stderr.write(`Error while executing command: ${err}\n`);
+        }
+      });
+  });
 }
-
-const config = Promise.all([
-  getConfig(GLOBAL_CONFIG_FILE, path.resolve(process.cwd(), '..')),
-  getConfig(CONFIG_FILE, process.cwd()),
-]).then((result) => (args.global ?
-    { ...BASE_CONFIG, ...result[0].data } :
-    { ...BASE_CONFIG, ...result[0].data, ...result[1].data })
-);
-
-let resolvedArgs = args;
-
-if (typeof command.getArgs === 'function' || Array.isArray(command.args)) {
-  resolvedArgs = config.then((cfg) => checkArgs(command, args, cfg));
-}
-
-Promise.all([
-  Promise.resolve(resolvedArgs),
-  config,
-]).then((result) => {
-  args = result[0];
-  const cfg = result[1];
-  Promise.resolve(command.exec(args, cfg))
-    .then(results => {
-      if (typeof command.print === 'function') {
-        command.print(results, args, cfg);
-      }
-    })
-    .catch(err => {
-      if (typeof command.catch === 'function') {
-        command.catch(err, args, cfg);
-      } else {
-        process.stderr.write(`Error while executing command: ${err}\n`);
-      }
-    });
-});
