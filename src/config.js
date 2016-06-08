f1ddb48911a3aab6567c7ee89dc6a094434d821d
Daniel Wagner <dawagner@adobe.com>
2016-06-08 09:51:46 +0200

    Added config module - used to find out and cache local application config

---
+++ src/config.js
@@ -0,0 +1,46 @@
+import findup from 'findup-sync';
+import path from 'path';
+import winston from 'winston';
+import { arrayify } from './utils';
+import os from 'os';
+import resolve from 'resolve';
+
+// Iterate module parents to find parent npm package
+let parentPackage = module.parent;
+while (parentPackage != null && parentPackage.filename.indexOf('/cmdcli/lib/') !== -1) {
+  parentPackage = parentPackage.parent;
+}
+
+const parentDir = path.dirname(parentPackage.filename);
+winston.debug(`CONFIG: parentDir = ${parentDir}`);
+const packageFile = findup('package.json', { cwd: parentDir });
+winston.debug(`CONFIG: packageFile = ${packageFile}`);
+const configFile = findup('.cmdclirc.json', { cwd: parentDir });
+winston.debug(`CONFIG: configFile = ${configFile}`);
+
+export const requireFn = (name) => {
+  // This searches up from the specified package.json file, making sure
+  // the config option behaves as expected. See issue #56.
+  const src = resolve.sync(name, { basedir: path.dirname(packageFile) });
+  return require(src);
+};
+
+export const configObject = {
+  ...requireFn(packageFile),
+  ...requireFn(configFile),
+};
+
+export const binEntryPoint = configObject.bin && Object.keys(configObject.bin).length ?
+  Object.keys(configObject.bin)[0] :
+  null;
+
+export const pattern = arrayify(configObject.commandsPattern || ['cmdcli-*', 'cmd-cli-*']);
+const scope = arrayify(configObject.commandsScope ||
+  ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']);
+export const LOCAL_FOLDERS = arrayify(configObject.localCommands || []);
+export const CONFIG_FILE = configObject.configFile || '.clirc';
+export const GLOBAL_CONFIG_FILE = `${os.homedir()}/${CONFIG_FILE}`;
+export const BASE_CONFIG = configObject.baseConfig || {};
+export const names = scope.reduce((result, prop) => (
+  result.concat(Object.keys(configObject[prop] || {}))
+), []);
