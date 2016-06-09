936cb9f4392ec94a949b48ecee0e2a7acce8d1b8
Daniel Wagner <dawagner@adobe.com>
2016-06-09 11:02:00 +0200

    Added const properties for logging

--- src/const.js
+++ src/const.js
@@ -4,3 +4,14 @@ export const DEFAULT_PACKAGE_SCOPE =
   ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies'];
 
 export const DEFAULT_PACKAGE_PATTERN = ['cmdcli-*', 'cmd-cli-*'];
+
+export const LOG_LEVELS = [
+  'error',
+  'warn',
+  'info',
+  'verbose',
+  'debug',
+  'silly',
+];
+
+export const DEFAULT_LOG_LEVEL = 2;
